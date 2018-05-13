c08e69498c551dec5994441682bfa1ace3519dc6
Arjen Meek <arjen@meeknet.nl>
2018-05-13 21:33:47 +0000

    Move query logic into new StatementQuery class

--- crunchyserver/controllers.py
+++ crunchyserver/controllers.py
@@ -2,9 +2,51 @@ from pyramid.view import view_config
 from uuid import UUID
 
 from crunchylib.exceptions import GeneralError
-from crunchylib.utility import deserialize_value
+from crunchylib.utility import deserialize_value, get_value_type
 
 from .models import Statement
+
+
+class StatementQuery(object):
+
+    def __init__(self, db):
+        self.db = db
+        self.aliases = {'main': Statement}
+        self.q = self.db.query(self.aliases['main'])
+
+    def _parse_reference(self, reference, object_type=None):
+        if ':' in reference:
+            value = deserialize_value(reference)
+        elif '.' in reference:
+            alias_name, attribute_name = reference.split('.')
+            if attribute_name == 'object' and object_type is not None:
+                attribute_name = 'object_{}'.format(object_type)
+            if not alias_name in self.aliases:
+                raise GeneralError("Unknown alias name: {}".format(alias_name))
+            value = getattr(self.aliases[alias_name], attribute_name)
+        else:
+            raise GeneralError("Invalid reference: {}".format(reference))
+        return value
+
+    def apply_filter(self, lhs_str, op_str, rhs_str=None):
+        rhs_type = None
+        if rhs_str is None:
+            rhs = None
+        else:
+            rhs = self._parse_reference(rhs_str)
+            if ':' in rhs_str:
+                rhs_type = get_value_type(rhs)
+        lhs = self._parse_reference(lhs_str, object_type=rhs_type)
+
+        if op_str == 'eq':
+            self.q = self.q.filter(lhs==rhs)
+            print('filter: {} == {}'.format(lhs, rhs))
+        else:
+            raise GeneralError("Unknown filter operation: {}".format(op_str))
+
+    def all(self):
+        statements = self.q.all()
+        return statements
 
 
 class BaseController(object):
@@ -25,6 +67,19 @@ class StatementController(BaseController):
         self.db = self.request.find_service(name='db')
         self.statements = self.request.find_service(name='statement_repository')
 
+    def _parse_filter_string(self, filter_string):
+        parts = filter_string.split(',')
+        if len(parts) == 2:
+            lhs, op = parts
+            rhs = None
+        elif len(parts) == 3:
+            lhs, op, rhs = parts
+        else:
+            raise GeneralError("Invalid filter string: {}".format(filter_string))
+
+        return lhs, op, rhs
+
+
     def parse_uuid_reference(self, reference):
         """Deserialize the reference if it's a UUID, raise an exception otherwise."""
         uuid_ = deserialize_value(reference)
@@ -36,7 +91,14 @@ class StatementController(BaseController):
     @view_config(route_name='find_statements', renderer='json')
     def find_statements(self):
         """Return multiple Statements."""
-        statements = self.statements.find()
+        qc = StatementQuery(self.db)
+
+        filter_strings = self.request.GET.getall('filter')
+        for fs in filter_strings:
+            lhs, op, rhs = self._parse_filter_string(fs)
+            qc.apply_filter(lhs, op, rhs)
+
+        statements = qc.all()
         return statements
 
     @view_config(route_name='get_statement', renderer='json')
