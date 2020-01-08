5c06099ffa66666aa8635218f691523a5f3b325e
Arjen Meek <arjen@meeknet.nl>
2020-01-08 21:45:03 +0000

    Add very basic authentication support
    Also authorization support.
    But no actual meaningful checking yet: essentially allow everything.

--- crunchyserver/__init__.py
+++ crunchyserver/__init__.py
@@ -1,8 +1,33 @@
 from pyramid.config import Configurator
+from pyramid.authentication import BasicAuthAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.security import ALL_PERMISSIONS, Allow, Authenticated, forget
+from pyramid.view import forbidden_view_config
+from pyramid.httpexceptions import HTTPUnauthorized
 from sqlalchemy import engine_from_config
 
 from .models import Base, init_model
 from .repositories import StatementRepository
+
+
+def forbidden_view(request):
+    """Trigger client to send basic HTTP auth info"""
+    if request.authenticated_userid is None:
+        response = HTTPUnauthorized()
+        response.headers.update(forget(request))
+        return response
+    return HTTPForbidden()
+
+def check_credentials(username, password, request):
+    """Always allows everything"""
+    return []
+
+class Root:
+    """Very simple root context"""
+    __acl__ = (
+        (Allow, Authenticated, ALL_PERMISSIONS),
+    )
+
 
 def main(settings):
     """Create and return a WSGI application."""
@@ -25,7 +50,13 @@ def main(settings):
         statement_repository = StatementRepository(request)
         return statement_repository
 
+    # Configure authentication / authorization
+    authn_policy = BasicAuthAuthenticationPolicy(check_credentials)
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(ACLAuthorizationPolicy())
     config.register_service_factory(statement_repository_factory, name='statement_repository')
+    config.set_root_factory(lambda request: Root())
+    config.add_forbidden_view(forbidden_view)
 
     config.add_route('find_statements', '/statements', request_method='GET')
     config.add_route('create_statements', '/statements', request_method='POST')
